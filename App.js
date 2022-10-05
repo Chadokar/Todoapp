ad59fd3b5f7154768ebb05aa5374f61dd07ba3f6
Shubham Chadokar <98835771+Chadokar@users.noreply.github.com>
2022-10-06 01:10:04 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Page from './components/page';
+import {View, StyleSheet, Text, Dimensions, Button, TouchableOpacity} from 'react-native';
 
-export default function App() {
+const deviceHeight = Dimensions.get("window").height
+
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header} >
+        <Text style={{fontSize:20, fontWeight:'700'}} >Food List</Text>
+      </View>
+      <Page/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    alignItems:'center',
+    justifyContent: 'flex-start',
+    
   },
+  header:{
+    alignItems:'center',
+    paddingTop:40,
+    backgroundColor:'white',
+    elevation:5,
+    width:'100%',
+    height: deviceHeight*0.11,
+  },
+
+
+
 });
+
+export default App;
